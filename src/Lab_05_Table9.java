03ddc639a2b74c10584382a1d75ef335d0c84a18
Pawan K <pawankesarapu@gmail.com>
2025-09-17 09:00:13 +0530

    17/09/2025- 9th table print using printf satatement

---
+++ src/Lab_05_Table9.java
@@ -0,0 +1,44 @@
+public class Lab_05_Table9 {
+    public static void main(String[] args) {
+        int table=9;
+
+        System.out.printf( "%d*1=%d",table,table*1);
+        System.out.println();
+        System.out.printf( "%d*2=%d",table,table*2);
+        System.out.println();
+        System.out.printf( "%d*3=%d",table,table*3);
+        System.out.println();
+        System.out.printf( "%d*4=%d",table,table*4);
+        System.out.println();
+        System.out.printf( "%d*5=%d",table,table*5);
+        System.out.println();
+        System.out.printf( "%d*6=%d",table,table*6);
+        System.out.println();
+        System.out.printf( "%d*7=%d",table,table*7);
+        System.out.println();
+        System.out.printf( "%d*8=%d",table,table*8);
+        System.out.println();
+        System.out.printf( "%d*9=%d",table,table*9);
+        System.out.println();
+        System.out.printf( "%d*10=%d",table,table*10);
+        System.out.println();
+        System.out.printf( "%d*11=%d",table,table*11);
+        System.out.println();
+        System.out.printf( "%d*12=%d",table,table*12);
+
+        /** 9*1=9
+         9*2=18
+         9*3=27
+         9*4=36
+         9*5=45
+         9*6=54
+         9*7=63
+         9*8=72
+         9*9=81
+         9*10=90
+         9*11=99
+         9*12=108
+                    **/
+
+         }
+}
